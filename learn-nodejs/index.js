d7a549a68a5d328065a11494bebffebde8a8df59
sugat jmgade <jamgdesugat42@gmail.com>
2026-02-12 15:00:11 +0530

    create server side reand

--- learn-nodejs/index.js
+++ learn-nodejs/index.js
@@ -1,33 +1,47 @@
 const express = require("express");
+const port = 8000;
 const user = require("./MOCK_DATA.json");
 
-const app =express();
-
-app.get("/api/user",(req,rep)=>{
-    return rep.json(user);
-})
-
+const app = express();
 
 app.get("/user",(req,rep)=>{
+    const html =`
+    <ul>
 
-const html =`
-
-<ul>
-${user.map(user)=>(
-    ``
-)}
-
-</ul>
-`;
-
-    return 
-
+   ${ user.map((user)=>(`<li>${user.first_name}</li>`)).join("")
+    }
+    
+    </ul>
+    `
+    rep.send(html);
 })
 
-app.listen(8000,(err)=>{
+
+app.get("/user/:id",(req,rep)=>{
+    const id =  Number(req.params.id);
+    const userid  = user.find((user)=>user.id === id);
+
+    const html = `
+    <h1>${userid.first_name}</h1>
+    <h2>${userid.last_name}</h2>
+        <h2>${userid.email}</h2>
+        <p>Gender:${userid.gender}</p>
+        <p>job:${userid.job_title}</p>
+
+    `
+    return rep.send(html);
+})
+
+
+
+
+const ok = (err)=>{
     if(err){
         console.log(err);
-    }else {
-        console.log("code is ok");
+    }else{
+        console.log("running ")
     }
-})
+
+}
+
+app.listen(port ,(err)=>{ok(err)} )
